90ce7c88a9d8d04dac0c54b0ad6ebfccd53318a4
Justin Dorfman <justindorfman@gmail.com>
2026-09-21 15:34:14 -0700

    Migrate wait.Poll/PollImmediate to wait.PollUntilContextTimeout (#1)

--- app/testing/testserver.go
+++ app/testing/testserver.go
@@ -209,14 +209,14 @@ func StartTestServerWithOptions(t *testing.T,
 	if err != nil {
 		return result, fmt.Errorf("failed to create a client: %v", err)
 	}
-	err = wait.Poll(100*time.Millisecond, 30*time.Second, func() (bool, error) {
+	err = wait.PollUntilContextTimeout(ctx, 100*time.Millisecond, 30*time.Second, false, func(ctx context.Context) (bool, error) {
 		select {
 		case err := <-errCh:
 			return false, err
 		default:
 		}
 
-		result := client.CoreV1().RESTClient().Get().AbsPath("/healthz").Do(context.TODO())
+		result := client.CoreV1().RESTClient().Get().AbsPath("/healthz").Do(ctx)
 		status := 0
 		result.StatusCode(&status)
 		if status == 200 {
